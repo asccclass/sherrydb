295afa61ad904f5fd69f161e7d731cabc9c13d88
Chih-Han Liu <andyliu@sinica.edu.tw>
2020-11-04 09:03:53 +0800

    <refector>修改範例

--- example.go
+++ example.go
@@ -17,17 +17,30 @@ func main() {
    dbconnect.DbLogin = os.Getenv("DBLOGIN")
    dbconnect.DbPasswd = os.Getenv("DBPASSWORD")
 
+   dbconnect.DBMS = os.Getenv("DBMS")
+   dbconnect.DbServer = "192.168.12.223"
+   dbconnect.DbPort = "3306"
+   dbconnect.DbName = "learnpoints"
+   dbconnect.DbLogin = "ascal"
+   dbconnect.DbPasswd = "webteam@2018"
+
    conn, err := SherryDB.NewSherryDB(dbconnect)
    if err != nil {
       fmt.Printf("%v", err)
       return 
    } 
-   orgs, err := conn.DoreFetchHash("select * from organizations")
+   var sysid string = ""
+   var course string = "(201919)資通安全管理法及維護計畫簡介說明會(第二場)"
+   var y int = 2019
+   if !conn.RowExists("select * from excela where Sysid=? and Year=? and CourseTitle=?", sysid, y, course) {
+      fmt.Println("No data.")
+   }
+   _, err = conn.DoreFetchHash("select * from excela")
    if err != nil {
       fmt.Printf("%v", err)
       return
    } 
-   fmt.Printf("%v\n", orgs)
+   // fmt.Printf("%v\n", orgs)
 /*
    for _, d := range orgs {
       fmt.Printf("%v\n", string(d))
